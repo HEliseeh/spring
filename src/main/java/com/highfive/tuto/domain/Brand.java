1282e359b5f53965679226c3c3648d061c0e52aa
Elisee <houinlinelisee@gamil.com>
2023-04-03 17:25:33 +0200

    ryt

--- src/main/java/com/highfive/tuto/domain/Brand.java
+++ src/main/java/com/highfive/tuto/domain/Brand.java
@@ -1,15 +1,16 @@
 package com.highfive.tuto.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+// import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Entity
-@Table(name= "model")
+@Table(name="model")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,14 +18,14 @@ import java.util.List;
 public class Brand {
     
     @Id
-    @GeneratedValue(strategy = GeneerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "name")
     private String name;
 
-    @OneToMany(mappedBy = "brand", cascade = cascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private list<Model> models = new ArrayList<>();
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Model> models = new ArrayList<>();
 
-    public Brand(string name) {this.name = name;}
+    public Brand(String name, long id) {this.name = name;}
 }
