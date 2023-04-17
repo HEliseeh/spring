f454858ca94a042028adecdba272cf9c077ca86b
Elisee <houinlinelisee@gmail.com>
2023-04-17 15:43:31 +0200

    h

--- src/main/java/com/highfive/tuto/web/rest/CrudResource.java
+++ src/main/java/com/highfive/tuto/web/rest/CrudResource.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.highfive.tuto.Service.CrudService;
 
